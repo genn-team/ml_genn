c015a5ff5690c63b79f790c6316b008f7ece293f
James Paul Turner <jamesturner246@gmail.com>
2020-04-25 18:19:55 +0100

    Roll back pygenn version requirement to 0.2.1, since that's what genn master is at now.

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.2.0",
     packages=find_packages(),
 
-    install_requires = ['tensorflow>=2.0', 'pygenn>=0.3', 'enum-compat']
+    install_requires = ['tensorflow>=2.0', 'pygenn>=0.2.1', 'enum-compat']
 )
