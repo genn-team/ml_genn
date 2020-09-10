a45d407faeac103263274c6b5ee5c3a9b86cc6ff
neworderofjamie <neworderofjamie@gmail.com>
2020-09-10 13:21:38 +0100

    bit of commenting in conv2d

--- tensor_genn/layers/conv2d_connection.py
+++ tensor_genn/layers/conv2d_connection.py
@@ -26,20 +26,26 @@ conv2d_init = create_custom_sparse_connect_init_snippet_class(
         lambda pars: UnsignedIntVector([int(pars[0]), int(pars[1]), int(pars[8]), int(pars[11])]))(),
 
     row_build_code='''
+    // Stash all parameters in registers
+    // **NOTE** this means parameters from group structure only get converted from float->int once
     const int conv_kh = $(conv_kh), conv_kw = $(conv_kw);
     const int conv_sh = $(conv_sh), conv_sw = $(conv_sw);
     const int conv_padh = $(conv_padh), conv_padw = $(conv_padw);
     const int conv_iw = $(conv_iw), conv_ic = $(conv_ic);
     const int conv_ow = $(conv_ow), conv_oh = $(conv_oh), conv_oc = $(conv_oc);
-    
+
+    // Convert presynaptic neuron ID to row, column and channel
     const int inRow = ($(id_pre) / conv_ic) / conv_iw;
     const int inCol = ($(id_pre) / conv_ic) % conv_iw;
     const int inChan = $(id_pre) % conv_ic;
+
+    // Calculate range of output rows and columns which this presynaptic neuron will connect to
     const int minOutRow = min(conv_oh, max(0, 1 + ((inRow + conv_padh - conv_kh) / conv_sh)));
     const int maxOutRow = min(conv_oh, max(0, 1 + ((inRow + conv_padh) / conv_sh)));
     const int minOutCol = min(conv_ow, max(0, 1 + ((inCol + conv_padw - conv_kw) / conv_sw)));
     const int maxOutCol = min(conv_ow, max(0, 1 + ((inCol + conv_padw) / conv_sw)));
 
+    // Loop through output rows, columns and channels
     for(int outRow = minOutRow; outRow != maxOutRow; outRow++) {
         const int strideRow = (outRow * conv_sh) - conv_padh;
         const int kernRow = inRow - strideRow;
@@ -47,6 +53,7 @@ conv2d_init = create_custom_sparse_connect_init_snippet_class(
             const int strideCol = (outCol * conv_sw) - conv_padw;
             const int kernCol = inCol - strideCol;
             for(int outChan = 0; outChan < conv_oc; outChan++) {
+                // Calculate postsynaptic index and add synapse
                 const int idPost = ((outRow * conv_ow * conv_oc) +
                                     (outCol * conv_oc) +
                                     outChan);
