9d0a53d3aeb6b0e4bfca07d4651a49400f9974ec
James Paul Turner <jamesturner246@gmail.com>
2021-01-19 13:23:40 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ml_genn",
-    version="0.3.0",
+    version="1.0.0",
     packages=find_packages(),
 
     install_requires = ['tensorflow>=2.0', 'pygenn>=0.4.1', 'enum-compat', 'tqdm']
