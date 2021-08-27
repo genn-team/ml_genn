88a044715c65bfac695a13bf52e5f990187d4d86
neworderofjamie <neworderofjamie@gmail.com>
2021-08-27 16:37:09 +0100

    simple resnet

--- tests/test_pipeline_depth.py
+++ tests/test_pipeline_depth.py
@@ -1,6 +1,6 @@
 from ml_genn.model import Model
-from ml_genn.layers import (AvePool2DConv2D, AvePool2DDense, Conv2D, Dense,
-                            InputLayer, FSReluNeurons, FSReluInputNeurons)
+from ml_genn.layers import (AvePool2D, AvePool2DConv2D, AvePool2DDense, Conv2D, 
+                            Dense, IdentitySynapses, InputLayer, FSReluNeurons, FSReluInputNeurons)
 
 def test_pipe_sequential():
     input = InputLayer("input", (28, 28, 1), neurons=FSReluInputNeurons())
@@ -23,7 +23,34 @@ def test_pipe_sequential():
     assert model.calc_pipeline_depth() == 4
 
 def test_pipe_resnet():
-    pass
-
-test_pipe_sequential()
+    input = InputLayer("input", (224, 224, 3), neurons=FSReluInputNeurons())
+    conv0 = Conv2D("conv0", 64, 7, 2, conv_padding='same', neurons=FSReluNeurons())
+    pool0 = AvePool2D("pool0", 3, neurons=FSReluNeurons())
+    conv0.connect([input])
+    pool0.connect([conv0])
+    
+    # block 1
+    block0_conv0 = Conv2D("block0_conv0", 64, 3, conv_padding='same', neurons=FSReluNeurons())
+    block0_conv1 = Conv2D("block0_conv1", 64, 3, conv_padding='same', neurons=FSReluNeurons())
+    block0_identity = IdentitySynapses()
+    block0_conv0.connect([pool0])
+    block0_conv1.connect([block0_conv0])
+    block0_identity.connect(pool0, block0_conv1)
+    
+    # block 2
+    block1_conv0 = Conv2D("block1_conv0", 64, 3, conv_padding='same', neurons=FSReluNeurons())
+    block1_conv1 = Conv2D("block1_conv1", 64, 3, conv_padding='same', neurons=FSReluNeurons())
+    block1_identity = IdentitySynapses()
+    block1_conv0.connect([block0_conv1])
+    block1_conv1.connect([block1_conv0])
+    block1_identity.connect(block0_conv1, block1_conv1)
+    
+    pool1 = AvePool2D("pool1", 56, neurons=FSReluNeurons())
+    pool1.connect([block1_conv1])
+    
+    # create model
+    model = Model([input], [pool1], name="test_pipe_resnet")
+    print(model.calc_pipeline_depth())
+    
+#test_pipe_sequential()
 test_pipe_resnet()
