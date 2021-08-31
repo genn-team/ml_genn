b5373c7f918d6c75f8bdf3ce4714b2491ad2b2c2
neworderofjamie <neworderofjamie@gmail.com>
2021-08-31 11:54:06 +0100

    Added branch identification code into FewSpike.pre_compile and added code to find shortest and longest paths through branching sections

--- ml_genn/converters/few_spike.py
+++ ml_genn/converters/few_spike.py
@@ -1,6 +1,9 @@
 import numpy as np
+import sys
 import tensorflow as tf
+from copy import copy
 from collections import namedtuple
+from six import iteritems
 
 from ml_genn.layers import FSReluNeurons
 from ml_genn.layers import FSReluInputNeurons
@@ -105,7 +108,72 @@ class FewSpike(object):
             return PreCompileOutput(max_activations={}, max_input=None)
 
     def pre_compile(self, mlg_model):
-        pass
+        # Dominators of the source node of the edge-reversed DAG is set containing just it 
+        # **NOTE** reversing topologically-sorted layers is a valid topological order for the edge-reversed DAG
+        dominators = {mlg_model.layers[-1]: set((mlg_model.layers[-1],))}
+
+        # Loop through layers in reverse topological order
+        for l in reversed(mlg_model.layers[:-1]):
+            # Get intersection of all this layers predecessor
+            # **NOTE** because we're operating on the edge-reversed graph, these are downstream targets
+            downstream_dominators = set.intersection(*(dominators[d.target()] 
+                                                       for d in l.downstream_synapses))
+            dominators[l] = downstream_dominators.union((l,))
+
+        # Create dictionary of STRICT dominators by removing layers from their own dominator sets 
+        strict_dominators = {l: d.difference((l,)) 
+                             for l, d in iteritems(dominators)}
+
+        # Loop through layers
+        branches = []
+        for l in mlg_model.layers:
+            # If graph diverges at this point
+            if len(l.downstream_synapses) > 1:
+                # Loop through split layer's strict dominators
+                rejoin_points = []
+                for i in strict_dominators[l]:
+                    # If i does not strictly dominate any of split layer's other strict dominators 
+                    if not any(i in strict_dominators[j] 
+                               for j in strict_dominators[l]):
+                        rejoin_points.append(i)
+                assert len(rejoin_points) == 1
+                branches.append((l, rejoin_points[0]))
+        
+        # Loop through branches
+        for split, rejoin in branches:
+            print("Branch:", split.name, "-", rejoin.name)
+            
+            # Slice out layers starting at split point
+            split_index = mlg_model.layers.index(split)
+            branch_layers = mlg_model.layers[split_index:]
+            
+            # Initialise layer distances to min and max size for all layers between split and rejoin
+            shortest_distance = {layer: (0 if layer == split else sys.maxsize)
+                                 for layer in branch_layers}
+            longest_distance = {layer: (0 if layer == split else -sys.maxsize)
+                                 for layer in branch_layers}
+            
+            # Initialiser list of best layer predecessors
+            best_short_predecessor = {layer: None for layer in branch_layers}
+            best_long_predecessor = {layer: None for layer in branch_layers}
+            
+            # Loop through layers in branching section
+            for v in branch_layers:
+                # Loop through layer's outgoing edges
+                for s in v.downstream_synapses:
+                    # If this results in a shorter route, relax
+                    u = s.target()
+                    if shortest_distance[u] > shortest_distance[v]:
+                        shortest_distance[u] = shortest_distance[v] + 1
+                        best_short_predecessor[u] = v
+                    
+                    # If this results in a longer route, relax
+                    if longest_distance[u] < longest_distance[v]:
+                        longest_distance[u] = longest_distance[v] + 1
+                        best_long_predecessor[u] = v
+            
+            # Return shortest distance to output
+            print(shortest_distance[rejoin], longest_distance[rejoin])
     
     def post_compile(self, mlg_model):
         # do not allow multiple input or output layers
