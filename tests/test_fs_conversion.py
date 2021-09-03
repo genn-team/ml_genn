3f4bee685bc5ec3c288c4100ed3cc83f9e6fde84
neworderofjamie <neworderofjamie@gmail.com>
2021-09-03 14:51:22 +0100

    completed test

--- tests/test_fs_conversion.py
+++ tests/test_fs_conversion.py
@@ -49,10 +49,29 @@ def test_delay_balancing():
     tf_y = tf_model(x).numpy() 
     
     # Check model does indeed have unity gain
-    assert np.allclose(x, tf_y)
+    assert np.array_equal(x, tf_y)
     
-    converter = mlg.converters.FewSpike(K=8, alpha=1.0)
+    # Convert model using few spike technique
+    converter = mlg.converters.FewSpike(K=8, alpha=8.0, norm_data=x)
     mlg_model = mlg.Model.convert_tf_model(tf_model, converter=converter)
+    
+    # Loop through inputs, taking into account pipeline depth
+    pipeline_depth = mlg_model.calc_pipeline_depth()
+    for i in range(len(x) + pipeline_depth):
+        # If there are inputs to present, set them as input batches
+        if i < len(x):
+            input = np.asarray([[x[i]]])
+            mlg_model.set_input_batch(input)
+        
+        # Reset and run model for K timesteps
+        mlg_model.reset()
+        mlg_model.step_time(8)
+        
+        # If outputs should be ready, pull and compare to x
+        if i >= pipeline_depth:
+            nrn = mlg_model.outputs[0].neurons.nrn
+            nrn.pull_var_from_device('Fx')
+            assert abs(nrn.vars['Fx'].view[0] - x[i - pipeline_depth]) <= ((4.0 * (2.0 ** -7)))
     
 if __name__ == '__main__':
     test_delay_balancing()
