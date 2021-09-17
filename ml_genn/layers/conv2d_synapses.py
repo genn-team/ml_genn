46da85fb9c3505583bef554a437a219a890faedd
James Paul Turner <jamesturner246@gmail.com>
2021-09-17 11:58:38 +0100

    Use GeNN built-in OneToOne model for identity synapses and built-in Conv2D for Conv2D synapses.

--- ml_genn/layers/conv2d_synapses.py
+++ ml_genn/layers/conv2d_synapses.py
@@ -1,74 +1,11 @@
 import numpy as np
 from math import ceil
-from pygenn.genn_model import create_custom_sparse_connect_init_snippet_class
-from pygenn.genn_model import (init_connectivity, init_var, 
-                               create_cmlf_class, create_cksf_class)
-from pygenn.genn_wrapper.StlContainers import UnsignedIntVector
+from pygenn.genn_model import init_connectivity, init_var
 
 from ml_genn.layers import ConnectivityType, PadMode
 from ml_genn.layers.base_synapses import BaseSynapses
 from ml_genn.layers.weight_update_models import signed_static_pulse
 from ml_genn.layers.helper import _get_param_2d
-
-conv2d_init = create_custom_sparse_connect_init_snippet_class(
-    'conv2d',
-
-    param_names=[
-        'conv_kh', 'conv_kw',
-        'conv_sh', 'conv_sw',
-        'conv_padh', 'conv_padw',
-        'conv_ih', 'conv_iw', 'conv_ic',
-        'conv_oh', 'conv_ow', 'conv_oc',
-    ],
-
-    calc_max_row_len_func=create_cmlf_class(
-        lambda num_pre, num_post, pars: (int(pars[0]) // int(pars[2])) * (int(pars[1]) // int(pars[3])) * int(pars[11]))(),
-
-    calc_kernel_size_func=create_cksf_class(
-        lambda pars: UnsignedIntVector([int(pars[0]), int(pars[1]), int(pars[8]), int(pars[11])]))(),
-
-    row_build_code='''
-    // Stash all parameters in registers
-    // **NOTE** this means parameters from group structure only get converted from float->int once
-    // **NOTE** if they're actually constant, compiler is still likely to treat them as constants rather than allocating registers
-    const int conv_kh = $(conv_kh), conv_kw = $(conv_kw);
-    const int conv_sh = $(conv_sh), conv_sw = $(conv_sw);
-    const int conv_padh = $(conv_padh), conv_padw = $(conv_padw);
-    const int conv_iw = $(conv_iw), conv_ic = $(conv_ic);
-    const int conv_ow = $(conv_ow), conv_oh = $(conv_oh), conv_oc = $(conv_oc);
-
-    // Convert presynaptic neuron ID to row, column and channel in conv input
-    const int inRow = ($(id_pre) / conv_ic) / conv_iw;
-    const int inCol = ($(id_pre) / conv_ic) % conv_iw;
-    const int inChan = $(id_pre) % conv_ic;
-
-    // Calculate range of output rows and columns which this presynaptic neuron connects to
-    const int minOutRow = min(conv_oh, max(0, 1 + ((inRow + conv_padh - conv_kh) / conv_sh)));
-    const int maxOutRow = min(conv_oh, max(0, 1 + ((inRow + conv_padh) / conv_sh)));
-    const int minOutCol = min(conv_ow, max(0, 1 + ((inCol + conv_padw - conv_kw) / conv_sw)));
-    const int maxOutCol = min(conv_ow, max(0, 1 + ((inCol + conv_padw) / conv_sw)));
-
-    // Loop through output rows, columns and channels
-    for(int outRow = minOutRow; outRow != maxOutRow; outRow++) {
-        const int strideRow = (outRow * conv_sh) - conv_padh;
-        const int kernRow = inRow - strideRow;
-        for(int outCol = minOutCol; outCol < maxOutCol; outCol++) {
-            const int strideCol = (outCol * conv_sw) - conv_padw;
-            const int kernCol = inCol - strideCol;
-            for(int outChan = 0; outChan < conv_oc; outChan++) {
-                // Calculate postsynaptic index and add synapse
-                const int idPost = ((outRow * conv_ow * conv_oc) +
-                                    (outCol * conv_oc) +
-                                    outChan);
-                $(addSynapse, idPost, kernRow, kernCol, inChan, outChan);
-            }
-        }
-    }
-    
-    // End the row
-    $(endRow);
-    ''',
-)
 
 class Conv2DSynapses(BaseSynapses):
 
@@ -119,7 +56,7 @@ class Conv2DSynapses(BaseSynapses):
             conv_padh = (conv_kh - 1) // 2
             conv_padw = (conv_kw - 1) // 2
 
-        conn_init = init_connectivity(conv2d_init, {
+        conn_init = init_connectivity('Conv2D', {
             'conv_kh': conv_kh, 'conv_kw': conv_kw,
             'conv_sh': conv_sh, 'conv_sw': conv_sw,
             'conv_padh': conv_padh, 'conv_padw': conv_padw,
