d991f51b5ddb4e801e60ba9a974b283ae41df6eb
neworderofjamie <neworderofjamie@gmail.com>
2021-10-04 14:00:06 +0100

    updated resnet18 to use correctly configured optimizer

--- examples/resnet18.py
+++ examples/resnet18.py
@@ -5,6 +5,7 @@ from ml_genn.utils import parse_arguments, raster_plot
 import numpy as np
 from six import iteritems
 from time import perf_counter
+from tensorflow_addons.optimizers import SGDW
 from tensorflow.keras import (callbacks, models, optimizers, regularizers)
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from ml_genn.utils import parse_arguments, raster_plot
@@ -22,12 +23,12 @@ def initializer_res(shape, dtype=None):
 initializer = "he_normal"
 initializer_res = "he_normal"
 
-def resnetFunctionalIdentity(input_layer, filters, regularizer, identity_cnn=False):
+def resnetFunctionalIdentity(input_layer, filters, identity_cnn=False):
     stride = 2 if identity_cnn else 1
     input_layer = tf.keras.layers.ReLU()(input_layer)
-    x = tf.keras.layers.Conv2D(filters, 3, padding='same', strides=stride, kernel_initializer=initializer_res, use_bias=False, activation='relu', kernel_regularizer=regularizer)(input_layer)
+    x = tf.keras.layers.Conv2D(filters, 3, padding='same', strides=stride, kernel_initializer=initializer_res, use_bias=False, activation='relu')(input_layer)
     x = tf.keras.layers.Dropout(0.2)(x)
-    x = tf.keras.layers.Conv2D(filters, 3, padding='same', strides=1, kernel_initializer=initializer_res, use_bias=False, kernel_regularizer=regularizer)(x)
+    x = tf.keras.layers.Conv2D(filters, 3, padding='same', strides=1, kernel_initializer=initializer_res, use_bias=False)(x)
     x = tf.keras.layers.Dropout(0.2)(x)
 
     if identity_cnn:
@@ -38,26 +39,26 @@ def resnetFunctionalIdentity(input_layer, filters, regularizer, identity_cnn=Fal
     x = tf.keras.layers.Add()([x, res])
     return x
 
-def resnet18(num_classes, regularizer):
+def resnet18(num_classes):
     input = tf.keras.layers.Input(shape=(32,32,3))
-    x = tf.keras.layers.Conv2D(64, 7, padding='same', strides=2, kernel_initializer=initializer, activation='relu', use_bias=False, kernel_regularizer=regularizer)(input)
+    x = tf.keras.layers.Conv2D(64, 7, padding='same', strides=2, kernel_initializer=initializer, activation='relu', use_bias=False)(input)
     x = tf.keras.layers.AveragePooling2D(3, strides=2)(x)
 
-    x = resnetFunctionalIdentity(x, 64, regularizer)
-    x = resnetFunctionalIdentity(x, 64,  regularizer)
+    x = resnetFunctionalIdentity(x, 64)
+    x = resnetFunctionalIdentity(x, 64)
     
-    x = resnetFunctionalIdentity(x, 128, regularizer, True)
-    x = resnetFunctionalIdentity(x, 128,  regularizer)
+    x = resnetFunctionalIdentity(x, 128, True)
+    x = resnetFunctionalIdentity(x, 128)
     
-    x = resnetFunctionalIdentity(x, 256, regularizer, True)
-    x = resnetFunctionalIdentity(x, 256,  regularizer)
+    x = resnetFunctionalIdentity(x, 256, True)
+    x = resnetFunctionalIdentity(x, 256)
     
-    x = resnetFunctionalIdentity(x, 512, regularizer, True)
-    x = resnetFunctionalIdentity(x, 512,  regularizer)
+    x = resnetFunctionalIdentity(x, 512, True)
+    x = resnetFunctionalIdentity(x, 512)
 
     x = tf.keras.layers.ReLU()(x)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dense(num_classes, activation='softmax', use_bias=False, kernel_regularizer=regularizer)(x)
+    x = tf.keras.layers.Dense(num_classes, activation='softmax', use_bias=False)(x)
     model = tf.keras.Model(inputs=input, outputs=x, name='Resnet18')
 
     return model
@@ -90,45 +91,47 @@ if __name__ == '__main__':
     y_test = y_test[:args.n_test_samples, 0]
     x_norm = x_train[np.random.choice(x_train.shape[0], args.n_norm_samples, replace=False)]
 
+    batch_size = 256
+    steps_per_epoch = x_train.shape[0] // batch_size
+
     # Check input size
     if x_train.shape[1] < 32 or x_train.shape[2] < 32:
         raise ValueError('input must be at least 32x32')
 
-    # If we should augment training data
-    if args.augment_training:
-        # Create image data generator
-        data_gen = ImageDataGenerator(horizontal_flip=True, validation_split=0.1)
+    # Create image data generator
+    data_gen = ImageDataGenerator(horizontal_flip=True, validation_split=0.1)
 
-        # Get training iterator
-        iter_train = data_gen.flow(x_train, y_train, batch_size=256, subset="training")
-        iter_validate = data_gen.flow(x_train, y_train, batch_size=256, subset="validation")
-
-    # Create L2 regularizer
-    regularizer = regularizers.l2(0.0001)
+    # Get training iterator
+    iter_train = data_gen.flow(x_train, y_train, batch_size=batch_size, subset="training")
+    iter_validate = data_gen.flow(x_train, y_train, batch_size=batch_size, subset="validation")
 
     # Creating the model
-    tf_model = resnet18(10, regularizer)
+    tf_model = resnet18(10)
     tf_model.build(input_shape = (None,32,32,3))
 
     if args.reuse_tf_model:
-        tf_model = models.load_model('resnet_tf_model')
+        tf_model.load_weights('resnet_tf_weights.h5')
     else:
-        optimizer = optimizers.SGD(lr=0.05, momentum=0.9)
+        # Configure matching schedules for learning rate and weight decay schedules
+        steps = [81 * steps_per_epoch, 122 * steps_per_epoch]
+        decay = [1.0, 0.1, 0.01]
+        lr_schedule = tf.optimizers.schedules.PiecewiseConstantDecay(
+            steps, [0.05 * d for d in decay])
+        wd_schedule = tf.optimizers.schedules.PiecewiseConstantDecay(
+            steps, [0.0001 * d for d in decay])
+        
+        optimizer = SGDW(learning_rate=lr_schedule, momentum=0.9, nesterov=True, weight_decay=wd_schedule)
         tf_model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-        fit_callbacks = [callbacks.LearningRateScheduler(schedule)]#,
-#                         callbacks.EarlyStopping(patience=10)]
+        fit_callbacks = []#,
+#                        callbacks.EarlyStopping(patience=10)]
     
         if args.record_tensorboard:
             fit_callbacks.append(callbacks.TensorBoard(log_dir="logs", histogram_freq=1))
 
-        if args.augment_training:
-            tf_model.fit(iter_train, validation_data=iter_validate, epochs=200, callbacks=fit_callbacks)
-        else:
-            tf_model.fit(x_train, y_train, batch_size=256, epochs=200, shuffle=True, 
-                         validation_split=0.1, callbacks=fit_callbacks)
-
-        models.save_model(tf_model, 'resnet_tf_model', save_format='h5')
+        tf_model.fit(iter_train, validation_data=iter_validate, epochs=200, callbacks=fit_callbacks)
+        
+        tf_model.save_weights('resnet_tf_weights.h5')
 
     tf_eval_start_time = perf_counter()
     tf_model.evaluate(x_test, y_test)
@@ -136,7 +139,6 @@ if __name__ == '__main__':
 
     # Create, suitable converter to convert TF model to ML GeNN
     converter = args.build_converter(x_norm, signed_input=True, K=10, norm_time=2500)
-
 
     # Convert and compile ML GeNN model
     mlg_model = Model.convert_tf_model(tf_model, converter=converter, connectivity_type=args.connectivity_type,
