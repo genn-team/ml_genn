13da331b5d337a30c99c5f424527095f50ccac28
James Paul Turner <jamesturner246@gmail.com>
2021-12-24 13:36:57 +0000

    use strides in test_conv2d

--- tests/test_conv2d.py
+++ tests/test_conv2d.py
@@ -58,7 +58,7 @@ def test_conv2d(in_size, in_chan, out_chan, kern_size, stride, pad, connect, req
     
     # Create TensorFlow model
     tf_model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(out_chan, kern_size, name='output', padding=pad,
+        tf.keras.layers.Conv2D(out_chan, kern_size, name='output', strides=stride, padding=pad,
                                use_bias=False, input_shape=(in_size, in_size, in_chan)),
     ], name=request.keywords.node.name)
     tf_model.set_weights([k])
