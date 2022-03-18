0d1e3201a8cc5c0a7c1955d0fb8b730a3b6d04d1
neworderofjamie <neworderofjamie@gmail.com>
2022-03-18 15:19:16 +0000

    explicitly capture output layer

--- ml_genn/examples/simple_mnist.py
+++ ml_genn/examples/simple_mnist.py
@@ -16,8 +16,8 @@ with model:
     input = InputLayer(IntegrateFireInput(v_thresh=5.0), 784)
     Layer(Dense(weight=np.load("weights_0_1.npy")), 
           IntegrateFire(v_thresh=5.0))
-    Layer(Dense(weight=np.load("weights_1_2.npy")), 
-          IntegrateFire(v_thresh=5.0, output="spike_count"))
+    output = Layer(Dense(weight=np.load("weights_1_2.npy")), 
+                   IntegrateFire(v_thresh=5.0, output="spike_count"))
 
 compiler = InferenceCompiler(dt=1.0, batch_size=BATCH_SIZE, 
                              evaluate_timesteps=100)
@@ -31,7 +31,7 @@ with compiled_model:
     # Evaluate model on numpy dataset
     start_time = perf_counter()
     accuracy = compiled_model.evaluate_numpy({input: testing_images * 0.01},
-                                             {model.layers[-1]: testing_labels})
+                                             {output: testing_labels})
     end_time = perf_counter()
     print(f"Accuracy = {100 * accuracy[model.layers[-1]]}%")
     print(f"Time = {end_time - start_time}s")
