d0b446f2574576d2303ccf53a37fd634596a4924
neworderofjamie <neworderofjamie@gmail.com>
2022-04-07 17:45:36 +0100

    dense can also be easily tested without a TF reference

---
+++ ml_genn/tests/test_dense.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pytest
+
+from ml_genn import InputLayer, Layer, SequentialNetwork
+from ml_genn.compilers import InferenceCompiler
+from ml_genn.neurons import IntegrateFire, BinarySpikeInput
+from ml_genn.connectivity import Dense
+
+@pytest.mark.parametrize(
+    "in_size, out_size",
+    [(5, 5),
+     (5, 5),
+     (5, 5,),
+     (5, 10)])
+def test_dense(in_size, out_size, request):
+    x = np.random.randint(0, 2, size=(1, in_size)).astype(np.float64)
+
+    # Generate and set weights
+    w = np.random.random_sample((in_size, out_size))
+    
+    # Calculate matrix product
+    y_true = np.matmul(x, w)
+
+    # Create sequential model
+    network = SequentialNetwork()
+    with network:
+        input = InputLayer(BinarySpikeInput(), in_size)
+        output = Layer(Dense(weight=w), 
+                       IntegrateFire(v_thresh=np.float64(np.finfo(np.float32).max), output="var"),
+                       out_size)
+
+    compiler = InferenceCompiler(evaluate_timesteps=2)
+    compiled_net = compiler.compile(network, request.keywords.node.name)
+
+    with compiled_net:
+        # Evaluate ML GeNN model
+        accuracy = compiled_net.evaluate_numpy({input: x},
+                                               {output: y_true},
+                                               "mean_square_error")
+    assert accuracy[output].result < 1e-03
