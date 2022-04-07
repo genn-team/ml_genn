2cca7aeae2f58cdabd9d6d9f0d2c522384f8321a
neworderofjamie <neworderofjamie@gmail.com>
2022-04-07 16:58:32 +0100

    special converter for connectivity tests

--- ml_genn_tf/tests/converter.py
+++ ml_genn_tf/tests/converter.py
@@ -1,37 +1,24 @@
-from ml_genn.layers import InputType
-from ml_genn.layers import IFNeurons
-from ml_genn.layers import SpikeInputNeurons
-from ml_genn.layers import PoissonInputNeurons
-from ml_genn.layers import IFInputNeurons
+import numpy as np
 
-class Converter(object):
-    def __init__(self, input_type=InputType.SPIKE):
-        self.input_type = InputType(input_type)
+from ml_genn.compilers import InferenceCompiler
+from ml_genn.neurons import BinarySpikeInput, IntegrateFire
 
+from ml_genn_tf.converters import Converter
+
+class Converter(Converter):
     def validate_tf_layer(self, tf_layer, config):
         # allow everything in test converter
         return
 
     def create_input_neurons(self, pre_compile_output):
-        if self.input_type == InputType.SPIKE:
-            return SpikeInputNeurons()
-        elif self.input_type == InputType.SPIKE_SIGNED:
-            return SpikeInputNeurons(signed_spikes=True)
-        elif self.input_type == InputType.POISSON:
-            return PoissonInputNeurons()
-        elif self.input_type == InputType.POISSON_SIGNED:
-            return PoissonInputNeurons(signed_spikes=True)
-        elif self.input_type == InputType.IF:
-            return IFInputNeurons()
+        return BinarySpikeInput()
 
-    def create_neurons(self, tf_layer, pre_compile_output):
-        return IFNeurons(threshold=1.0)
+    def create_neurons(self, tf_layer, pre_convert_output, is_output):
+        if is_output:
+            v_thresh = np.float64(np.finfo(np.float32).max)
+            return IntegrateFire(v_thresh=v_thresh, output="var")
+        else:
+            return IntegrateFire(v_thresh=1.0)
 
-    def pre_convert(self, tf_model):
-        pass
-    
-    def pre_compile(self, mlg_model):
-        pass
-
-    def post_compile(self, mlg_model):
-        pass
+    def create_compiler(self, **kwargs):
+        return InferenceCompiler(evaluate_timesteps=2, **kwargs)
