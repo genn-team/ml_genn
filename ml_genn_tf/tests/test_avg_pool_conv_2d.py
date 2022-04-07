9efe6a0af524e21283607f108e039a300bc936c4
neworderofjamie <neworderofjamie@gmail.com>
2022-04-07 17:06:13 +0100

    avg pool conv2d tests working

--- ml_genn_tf/tests/test_avg_pool_conv_2d.py
+++ ml_genn_tf/tests/test_avg_pool_conv_2d.py
@@ -5,38 +5,28 @@ import pytest
 from converter import Converter
 
 @pytest.mark.parametrize(
-    'in_size, in_chan, out_chan, pool_size, pool_strides, conv_size, conv_strides, conv_padding, connect', [
-        (20, 1, 1, 2, 2, 3, 1, 'valid', 'sparse'),
-        (20, 1, 1, 2, 2, 3, 1, 'valid', 'procedural'),
-        (20, 1, 1, 2, 2, 3, 1, 'valid', 'toeplitz'),
-        (20, 2, 2, 2, 2, 3, 1, 'valid', 'sparse'),
-        (20, 2, 2, 2, 2, 3, 1, 'valid', 'procedural'),
-        (20, 2, 2, 2, 2, 3, 1, 'valid', 'toeplitz'),
-        (20, 1, 1, 2, 2, 3, 1, 'same', 'sparse'),
-        (20, 1, 1, 2, 2, 3, 1, 'same', 'procedural'),
-        (20, 1, 1, 2, 2, 3, 1, 'same', 'toeplitz'),
-        (20, 2, 2, 2, 2, 3, 1, 'same', 'sparse'),
-        (20, 2, 2, 2, 2, 3, 1, 'same', 'procedural'),
-        (20, 2, 2, 2, 2, 3, 1, 'same', 'toeplitz'),
-        (20, 1, 1, 3, 3, 4, 1, 'valid', 'sparse'),
-        (20, 1, 1, 3, 3, 4, 1, 'valid', 'procedural'),
-        (20, 1, 1, 3, 3, 4, 1, 'valid', 'toeplitz'),
-        (20, 1, 1, 3, 4, 4, 1, 'valid', 'sparse'),
-        (20, 1, 1, 3, 4, 4, 1, 'valid', 'procedural'),
-        (20, 1, 1, 3, 4, 4, 1, 'valid', 'toeplitz'),
-        (20, 1, 1, 3, 3, 4, 1, 'same', 'sparse'),
-        (20, 1, 1, 3, 3, 4, 1, 'same', 'procedural'),
-        #(20, 1, 1, 3, 3, 4, 1, 'same', 'toeplitz'), # FAIL: toeplitz, even size conv kernel, same padding
-        (20, 1, 1, 3, 4, 4, 1, 'same', 'sparse'),
-        (20, 1, 1, 3, 4, 4, 1, 'same', 'procedural'),
-        #(20, 1, 1, 3, 4, 4, 1, 'same', 'toeplitz'), # FAIL: toeplitz, even size conv kernel, same padding
-    ])
-
-def test_avepool2d_conv2d(
-        in_size, in_chan, out_chan, pool_size, pool_strides,
-        conv_size, conv_strides, conv_padding, connect, request):
-    # Don't use all GPU memory for TF!
-    for gpu in tf.config.experimental.list_physical_devices('GPU'):
+    "in_size, in_chan, out_chan, pool_size, pool_strides, conv_size, conv_strides, conv_padding, prefer_in_memory_connect", 
+    [(20, 1, 1, 2, 2, 3, 1, "valid", True),
+     (20, 1, 1, 2, 2, 3, 1, "valid", False),
+     (20, 2, 2, 2, 2, 3, 1, "valid", True),
+     (20, 2, 2, 2, 2, 3, 1, "valid", False),
+     (20, 1, 1, 2, 2, 3, 1, "same", True),
+     (20, 1, 1, 2, 2, 3, 1, "same", False),
+     (20, 2, 2, 2, 2, 3, 1, "same", True),
+     (20, 2, 2, 2, 2, 3, 1, "same", False),
+     (20, 1, 1, 3, 3, 4, 1, "valid", True),
+     (20, 1, 1, 3, 3, 4, 1, "valid", False),
+     (20, 1, 1, 3, 4, 4, 1, "valid", True),
+     (20, 1, 1, 3, 4, 4, 1, "valid", False),
+     (20, 1, 1, 3, 3, 4, 1, "same", True),
+     (20, 1, 1, 3, 3, 4, 1, "same", False),
+     (20, 1, 1, 3, 4, 4, 1, "same", True),
+     (20, 1, 1, 3, 4, 4, 1, "same", False)])
+def test_avg_pool_conv_2d(in_size, in_chan, out_chan, pool_size, pool_strides,
+                          conv_size, conv_strides, conv_padding, 
+                          prefer_in_memory_connect, request):
+    # Don"t use all GPU memory for TF!
+    for gpu in tf.config.experimental.list_physical_devices("GPU"):
         tf.config.experimental.set_memory_growth(gpu, True)
 
     # Generate input tensor
@@ -45,11 +35,10 @@ def test_avepool2d_conv2d(
     # Create TensorFlow model
     tf_model = tf.keras.models.Sequential([
         tf.keras.layers.AveragePooling2D(
-            pool_size, strides=pool_strides, padding='valid',
+            pool_size, strides=pool_strides, padding="valid",
             input_shape=(in_size, in_size, in_chan)),
         tf.keras.layers.Conv2D(
-            out_chan, conv_size, strides=conv_strides, padding=conv_padding, use_bias=False),
-    ], name=request.keywords.node.name)
+            out_chan, conv_size, strides=conv_strides, padding=conv_padding, use_bias=False)])
 
     # Generate and set weights
     w = np.random.random_sample((conv_size, conv_size, in_chan, out_chan))
@@ -58,15 +47,18 @@ def test_avepool2d_conv2d(
     # Run TensorFlow model
     tf_y = tf_model([x]).numpy()
 
-    # Run ML GeNN model
-    mlg_model = mlg.Model.convert_tf_model(
-        tf_model, converter=Converter(), connectivity_type=connect)
-    mlg_model.outputs[0].neurons.set_threshold(np.float64(np.inf))
-    mlg_model.set_input_batch([x])
-    mlg_model.step_time(2)
+    # Convert and compile ML GeNN model
+    converter = Converter()
+    mlg_net, mlg_net_inputs, mlg_net_outputs = converter.convert(tf_model)
+    
+    compiler = converter.create_compiler(prefer_in_memory_connect=prefer_in_memory_connect)
+    compiled_net = compiler.compile(mlg_net, request.keywords.node.name, 
+                                    inputs=mlg_net_inputs, 
+                                    outputs=mlg_net_outputs)
 
-    nrn = mlg_model.outputs[0].neurons.nrn
-    nrn.pull_var_from_device('Vmem')
-    mlg_y = nrn.vars['Vmem'].view.reshape(tf_y.shape)
-
-    assert(np.allclose(mlg_y, tf_y, atol=0.0, rtol=1.0e-3))
+    with compiled_net:
+        # Evaluate ML GeNN model
+        accuracy = compiled_net.evaluate_numpy({mlg_net_inputs[0]: x},
+                                               {mlg_net_outputs[0]: tf_y},
+                                               "mean_square_error")
+    assert accuracy[mlg_net_outputs[0]].result < 1e-03
