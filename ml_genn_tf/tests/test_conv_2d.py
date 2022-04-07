5a8368cbd4504bcef7606fb5f49b01c8b9a35ad4
neworderofjamie <neworderofjamie@gmail.com>
2022-04-07 17:01:48 +0100

    conv tests working

--- ml_genn_tf/tests/test_conv_2d.py
+++ ml_genn_tf/tests/test_conv_2d.py
@@ -5,35 +5,26 @@ import pytest
 from converter import Converter
 
 @pytest.mark.parametrize(
-    'in_size, in_chan, out_chan, conv_size, conv_strides, conv_padding, connect', [
-        (12, 1, 1, 3, 1, 'valid', 'sparse'),
-        (12, 1, 1, 3, 1, 'valid', 'procedural'),
-        (12, 1, 1, 3, 1, 'valid', 'toeplitz'),
-        (12, 1, 1, 3, 1, 'same', 'sparse'),
-        (12, 1, 1, 3, 1, 'same', 'procedural'),
-        (12, 1, 1, 3, 1, 'same', 'toeplitz'),
-        (12, 2, 1, 3, 1, 'valid', 'sparse'),
-        (12, 2, 1, 3, 1, 'valid', 'procedural'),
-        (12, 2, 1, 3, 1, 'valid', 'toeplitz'),
-        (12, 1, 2, 3, 1, 'valid', 'sparse'),
-        (12, 1, 2, 3, 1, 'valid', 'procedural'),
-        (12, 1, 2, 3, 1, 'valid', 'toeplitz'),
-        (12, 2, 2, 3, 1, 'valid', 'sparse'),
-        (12, 2, 2, 3, 1, 'valid', 'procedural'),
-        (12, 2, 2, 3, 1, 'valid', 'toeplitz'),
-        (12, 1, 1, 3, 2, 'valid', 'sparse'),
-        (12, 1, 1, 3, 2, 'valid', 'procedural'),
-        (12, 1, 1, 3, 2, 'valid', 'toeplitz'),
-        (12, 1, 1, 3, 2, 'same', 'sparse'),
-        (12, 1, 1, 3, 2, 'same', 'procedural'),
-        (12, 1, 1, 3, 2, 'same', 'toeplitz'),
-    ])
+    "in_size, in_chan, out_chan, conv_size, conv_strides, conv_padding, prefer_in_memory_connect", 
+    [(12, 1, 1, 3, 1, "valid", True),
+     (12, 1, 1, 3, 1, "valid", False),
+     (12, 1, 1, 3, 1, "same", True),
+     (12, 1, 1, 3, 1, "same", False),
+     (12, 2, 1, 3, 1, "valid", True),
+     (12, 2, 1, 3, 1, "valid", False),
+     (12, 1, 2, 3, 1, "valid", True),
+     (12, 1, 2, 3, 1, "valid", False),
+     (12, 2, 2, 3, 1, "valid", True),
+     (12, 2, 2, 3, 1, "valid", False),
+     (12, 1, 1, 3, 2, "valid", True),
+     (12, 1, 1, 3, 2, "valid", False),
+     (12, 1, 1, 3, 2, "same", True),
+     (12, 1, 1, 3, 2, "same", False)])
 
-def test_conv2d(
-        in_size, in_chan, out_chan, conv_size, conv_strides,
-        conv_padding, connect, request):
-    # Don't use all GPU memory for TF!
-    for gpu in tf.config.experimental.list_physical_devices('GPU'):
+def test_conv_2d(in_size, in_chan, out_chan, conv_size, conv_strides,
+                 conv_padding, prefer_in_memory_connect, request):
+    # Don"t use all GPU memory for TF!
+    for gpu in tf.config.experimental.list_physical_devices("GPU"):
         tf.config.experimental.set_memory_growth(gpu, True)
     
     # Generate input tensor
@@ -43,8 +34,7 @@ def test_conv2d(
     tf_model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(
             out_chan, conv_size, strides=conv_strides, padding=conv_padding, use_bias=False,
-            input_shape=(in_size, in_size, in_chan)),
-    ], name=request.keywords.node.name)
+            input_shape=(in_size, in_size, in_chan))])
 
     # Generate and set weights
     w = np.random.random_sample((conv_size, conv_size, in_chan, out_chan))
@@ -53,15 +43,18 @@ def test_conv2d(
     # Run TensorFlow model
     tf_y = tf_model([x]).numpy()
 
-    # Run ML GeNN model
-    mlg_model = mlg.Model.convert_tf_model(
-        tf_model, converter=Converter(), connectivity_type=connect)
-    mlg_model.outputs[0].neurons.set_threshold(np.float64(np.inf))
-    mlg_model.set_input_batch([x])
-    mlg_model.step_time(2)
+    # Convert and compile ML GeNN model
+    converter = Converter()
+    mlg_net, mlg_net_inputs, mlg_net_outputs = converter.convert(tf_model)
+    
+    compiler = converter.create_compiler(prefer_in_memory_connect=prefer_in_memory_connect)
+    compiled_net = compiler.compile(mlg_net, request.keywords.node.name, 
+                                    inputs=mlg_net_inputs, 
+                                    outputs=mlg_net_outputs)
 
-    nrn = mlg_model.outputs[0].neurons.nrn
-    nrn.pull_var_from_device('Vmem')
-    mlg_y = nrn.vars['Vmem'].view.reshape(tf_y.shape)
-
-    assert(np.allclose(mlg_y, tf_y, atol=0.0, rtol=1.0e-3))
+    with compiled_net:
+        # Evaluate ML GeNN model
+        accuracy = compiled_net.evaluate_numpy({mlg_net_inputs[0]: x},
+                                               {mlg_net_outputs[0]: tf_y},
+                                               "mean_square_error")
+    assert accuracy[mlg_net_outputs[0]].result < 1e-03
