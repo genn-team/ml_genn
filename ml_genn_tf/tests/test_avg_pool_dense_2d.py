aacf8f1e533f0ceec9851aa4cfb396285d1b7781
neworderofjamie <neworderofjamie@gmail.com>
2022-04-07 17:13:48 +0100

    avg pool dense tests working

--- ml_genn_tf/tests/test_avg_pool_dense_2d.py
+++ ml_genn_tf/tests/test_avg_pool_dense_2d.py
@@ -5,22 +5,19 @@ import pytest
 from converter import Converter
 
 @pytest.mark.parametrize(
-    'in_size, in_chan, out_size, pool_size, pool_strides, connect', [
-        (20, 1, 10, 2, 2, 'sparse'),
-        (20, 1, 10, 2, 2, 'procedural'),
-        (20, 2, 10, 2, 2, 'sparse'),
-        (20, 2, 10, 2, 2, 'procedural'),
-        (20, 1, 10, 3, 3, 'sparse'),
-        (20, 1, 10, 3, 3, 'procedural'),
-        (20, 1, 10, 2, 3, 'sparse'),
-        (20, 1, 10, 2, 3, 'procedural'),
-    ])
-
-def test_avepool2d_dense(
-        in_size, in_chan, out_size, pool_size,
-        pool_strides, connect, request):
-    # Don't use all GPU memory for TF!
-    for gpu in tf.config.experimental.list_physical_devices('GPU'):
+    "in_size, in_chan, out_size, pool_size, pool_strides, prefer_in_memory_connect", 
+    [(20, 1, 10, 2, 2, True),
+     (20, 1, 10, 2, 2, False),
+     (20, 2, 10, 2, 2, True),
+     (20, 2, 10, 2, 2, False),
+     (20, 1, 10, 3, 3, True),
+     (20, 1, 10, 3, 3, False),
+     (20, 1, 10, 2, 3, True),
+     (20, 1, 10, 2, 3, False)])
+def test_avg_pool_dense_2d(in_size, in_chan, out_size, pool_size,
+                           pool_strides, prefer_in_memory_connect, request):
+    # Don"t use all GPU memory for TF!
+    for gpu in tf.config.experimental.list_physical_devices("GPU"):
         tf.config.experimental.set_memory_growth(gpu, True)
 
     # Generate input tensor
@@ -29,11 +26,10 @@ def test_avepool2d_dense(
     # Create TensorFlow model
     tf_model = tf.keras.models.Sequential([
         tf.keras.layers.AveragePooling2D(
-            pool_size, strides=pool_strides, padding='valid',
+            pool_size, strides=pool_strides, padding="valid",
             input_shape=(in_size, in_size, in_chan)),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(out_size, use_bias=False),
-    ], name=request.keywords.node.name)
+        tf.keras.layers.Dense(out_size, use_bias=False)])
 
     # Generate and set weights
     w = tf_model.get_weights()[0]
@@ -43,15 +39,18 @@ def test_avepool2d_dense(
     # Run TensorFlow model
     tf_y = tf_model([x]).numpy()
 
-    # Run ML GeNN model
-    mlg_model = mlg.Model.convert_tf_model(
-        tf_model, converter=Converter(), connectivity_type=connect)
-    mlg_model.outputs[0].neurons.set_threshold(np.float64(np.inf))
-    mlg_model.set_input_batch([x])
-    mlg_model.step_time(2)
+    # Convert and compile ML GeNN model
+    converter = Converter()
+    mlg_net, mlg_net_inputs, mlg_net_outputs = converter.convert(tf_model)
+    
+    compiler = converter.create_compiler(prefer_in_memory_connect=prefer_in_memory_connect)
+    compiled_net = compiler.compile(mlg_net, request.keywords.node.name, 
+                                    inputs=mlg_net_inputs, 
+                                    outputs=mlg_net_outputs)
 
-    nrn = mlg_model.outputs[0].neurons.nrn
-    nrn.pull_var_from_device('Vmem')
-    mlg_y = nrn.vars['Vmem'].view.reshape(tf_y.shape)
-
-    assert(np.allclose(mlg_y, tf_y, atol=0.0, rtol=1.0e-3))
+    with compiled_net:
+        # Evaluate ML GeNN model
+        accuracy = compiled_net.evaluate_numpy({mlg_net_inputs[0]: x},
+                                               {mlg_net_outputs[0]: tf_y},
+                                               "mean_square_error")
+    assert accuracy[mlg_net_outputs[0]].result < 1e-03
