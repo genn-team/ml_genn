0cd9b0d4d54bf768fefb8788bd9a51394834d351
neworderofjamie <neworderofjamie@gmail.com>
2022-08-23 10:19:22 +0100

    thought more about ``ExampleFilter`` design

--- ml_genn/ml_genn/utils/filter.py
+++ ml_genn/ml_genn/utils/filter.py
@@ -3,11 +3,11 @@ import numpy as np
 from numbers import Number
 from typing import Sequence
 
-class BatchFilter:
+class ExampleFilter:
     def __init__(self, f):
-        # If filter is set to a single boolean value, use it directly as mask
-        if isinstance(f, bool):
-            self._mask = f
+        # If no filter is specified
+        if f is None:
+            self._mask = None
         # Otherwise, if filter is specified with array
         elif isinstance(f, (np.ndarray, Sequence)):
             # Convert to numpy
@@ -25,28 +25,36 @@ class BatchFilter:
                 self._mask[array] = True
             else:
                 raise RuntimeError("Unsupported object in bacth filter")
+        elif isinstance(f, (int, np.integer)):
+            self._mask = np.zeros(f + 1, dtype=bool)
+            self._mask[f] = True
         else:
             raise RuntimeError("Unsupported batch filter format")
 
-    def __getitem__(self, key):
-        if isinstance(self._mask, bool):
-            return self._mask
-        elif key < len(self._mask):
-            return self._mask[key]
+    def get_batch_mask(self, batch, batch_size):
+        # If no mask is specified, return array of Trues
+        if self._mask is None:
+            return np.ones(batch_size, dtype=bool)
         else:
-            return False
-    
+            # Get indices of examples at beginning and end of batch
+            batch_start = batch * batch_size
+            batch_end = batch_start + batch_size
+                
+            # Extract slice of mask
+            mask_slice = self._mask[batch_start:batch_end]
+
+            # Pad with False out to full batch size
+            return np.pad(mask_slice, (0, batch_size - len(mask_slice)),
+                          constant_values=False)
+
 class NeuronFilter:
     def __init__(self, f, shape):
         # Normalize shape
         shape = shape if isinstance(shape, Sequence) else (shape,)
-        
-        # If filter is set to a single boolean value, use it directly as mask
-        if isinstance(f, bool):
-            self._mask = f            
-        # Otherwise, if filter is specified as a slice, create   
+                 
+        # If filter is specified as a slice, create   
         # mask and set elements specified by slice to True
-        elif isinstance(f, slice):
+        if isinstance(f, slice):
             if len(shape) != 1:
                 raise RuntimeError("Neuron filters can only be specified "
                                    "as a slice if their shape is 1D")
@@ -86,9 +94,3 @@ class NeuronFilter:
                     self._mask[tuple(array)] = True
         else:
             raise RuntimeError("Unsupported neuronfilter format")
-           
-    def __getitem__(self, key):
-        if isinstance(self._mask, bool):
-            return self._mask
-        else:
-            return self._mask[key]
--- ml_genn/tests/utils/test_filter.py
+++ ml_genn/tests/utils/test_filter.py
@@ -1,35 +1,32 @@
 import numpy as np
 
-from ml_genn.utils.filter import BatchFilter, NeuronFilter
+from ml_genn.utils.filter import ExampleFilter, NeuronFilter
 
 from pytest import raises
 
 test_mask_1d = [True, False, True, False, True]
 test_mask_2d = [[True, False, True], [False, False, False]]
 
-def test_batch_bool():
-    f = BatchFilter(True)
-    assert f[2] == True
+def test_example_none():
+    f = ExampleFilter(None)
+    assert np.array_equal(f.get_batch_mask(20, 2), [True, True])
 
-def test_batch_bool_array():
-    f = BatchFilter(test_mask_1d)
+def test_example_bool_array():
+    f = ExampleFilter(test_mask_1d)
 
-def test_batch_int_array():
-    f = BatchFilter([0, 2, 4])
-    assert np.array_equal(f._mask, test_mask_1d)
+def test_example_int_array():
+    f = ExampleFilter([0, 2, 4])
+    
+    assert np.array_equal(f.get_batch_mask(0, 2), [True, False])
 
-def test_batch_other_object_array():
+def test_example_other_object_array():
     with raises(RuntimeError):
-        f = BatchFilter(["hello", 2, 4])
+        f = ExampleFilter(["hello", 2, 4])
     
-def test_batch_beyond_mask():
-    f = BatchFilter(test_mask_1d)
-    assert not f[10]
+def test_example_beyond_mask():
+    f = ExampleFilter(test_mask_1d)
+    assert np.array_equal(f.get_batch_mask(2, 2), [True, False])
 
-def test_neuron_1d_bool():
-    f = NeuronFilter(True, 100)
-    assert f[10] == True
-    
 def test_neuron_1d_slice():
     f = NeuronFilter(slice(0, None, 2), 5)
     assert np.array_equal(f._mask, test_mask_1d)
@@ -53,9 +50,6 @@ def test_neuron_1d_other_object_array():
     with raises(RuntimeError):
         f = NeuronFilter(["hello", 2, 4], 5)
         
-def test_neuron_2d_bool():
-    f = NeuronFilter(True, (2, 3))
-    assert f[1, 2] == True
 
 def test_neuron_2d_slice():
     f = NeuronFilter((slice(0, None, 2), slice(0, None, 2)), (2, 3))
