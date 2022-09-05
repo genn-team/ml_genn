29dac0a785c5adf6edf45554527c808f01da32fc
neworderofjamie <neworderofjamie@gmail.com>
2022-09-05 11:10:04 +0100

    tweaked optimiser API

--- ml_genn/ml_genn/optimisers/adam.py
+++ ml_genn/ml_genn/optimisers/adam.py
@@ -32,26 +32,27 @@ class Adam(Optimiser):
     beta2 = ConstantValueDescriptor()
     epsilon = ConstantValueDescriptor()
 
-    def __init__(self, alpha, beta1=0.9, beta2=0.999, epsilon=1e-8):
+    def __init__(self, alpha=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
         self.alpha = alpha
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
     
-    def set_learning_rate(self, genn_cu, step, alpha):
+    def set_step(self, genn_cu, step):
         assert step >= 0
         moment_scale_1 = 1.0 / (1.0 - (self.beta1 ** (step + 1)))
         moment_scale_2 = 1.0 / (1.0 - (self.beta2 ** (step + 1)))
 
-        genn_pop.extra_global_params["Alpha"].view[:] = alpha
+        genn_pop.extra_global_params["Alpha"].view[:] = self.alpha
         genn_pop.extra_global_params["MomentScale1"].view[:] = moment_scale_1
         genn_pop.extra_global_params["MomentScale2"].view[:] = moment_scale_2
     
     def get_model(self, gradient_ref, var_ref):
-        return CustomUpdateModelModel(genn_model,
-                        {"Beta1": self.beta1, "Beta2": self.beta2,
-                         "Epsilon": self.epsilon},
-                        {"M": 0.0, "V": 0.0},
-                        {"Gradient": gradient_ref, "Variable": var_ref},
-                        {"Alpha": self.alpha, "FirstMomentScale": 0.0,
-                         "SecondMomentScale": 0.0}))
+        return CustomUpdateModel(
+            genn_model,
+            {"Beta1": self.beta1, "Beta2": self.beta2,
+             "Epsilon": self.epsilon},
+            {"M": 0.0, "V": 0.0},
+            {"Gradient": gradient_ref, "Variable": var_ref},
+            {"Alpha": self.alpha, "FirstMomentScale": 0.0, 
+             "SecondMomentScale": 0.0})
