0aa37410320bf23c31b895f273a5bc53ec4b54ab
neworderofjamie <neworderofjamie@gmail.com>
2022-09-06 14:54:26 +0100

    hook together pattern recognition

--- ml_genn_eprop/examples/pattern_recognition.py
+++ ml_genn_eprop/examples/pattern_recognition.py
@@ -20,27 +20,6 @@ IN_GROUP_SIZE = 4
 IN_ACTIVE_ISI = 10
 IN_ACTIVE_INTERVAL = 200
 
-network = Network()
-with network:
-    # Populations
-    input = Population(SpikeInput(max_spikes=100), NUM_INPUT)
-    hidden = Population(LeakyIntegrateFire(v_thresh=0.61, tau_mem=20.0,
-                                           tau_refrac=5.0, 
-                                           relative_reset=True,
-                                           integrate_during_refrac=True),
-                        NUM_HIDDEN)
-    output = Population(LeakyIntegrate(tau_mem=20.0, output="var"),
-                        NUM_OUTPUT)
-    
-    # Connections
-    Connection(input, hidden, Dense(Normal(sd=1.0 / np.sqrt(NUM_INPUT))))
-    Connection(hidden, hidden, Dense(Normal(sd=1.0 / np.sqrt(NUM_HIDDEN))))
-    Connection(hidden, output, Dense(Normal(sd=1.0 / np.sqrt(NUM_INPUT))))
-
-compiler = EPropCompiler(example_timesteps=1000, losses="mean_square_error",
-                         optimiser=Adam(), c_reg=3.0)
-compiled_net = compiler.compile(network)
-
 # Pick random phases and outputs for the three frequency components 
 # components used to generate patterns for each output neuron
 output_phase = np.random.random((NUM_OUTPUT, NUM_FREQ_COMP)) * 2.0 * np.pi
@@ -63,7 +42,6 @@ for i in range(NUM_OUTPUT):
 # Determine which group each input neuron is in
 in_group = np.arange(NUM_INPUT, dtype=int) // IN_GROUP_SIZE
 
-
 # Fill matrix rows with base spike times
 num_spikes_per_neuron = IN_ACTIVE_INTERVAL // IN_ACTIVE_ISI
 in_spike_times = np.empty((NUM_INPUT, num_spikes_per_neuron))
@@ -72,3 +50,41 @@ in_spike_times[:] = np.arange(0, IN_ACTIVE_INTERVAL, IN_ACTIVE_ISI)
 # Shift each spike time by group start
 in_spike_times += np.reshape(in_group * IN_ACTIVE_INTERVAL, (NUM_INPUT, 1))
 
+# Create matching array of IDs
+in_spike_ids = np.repeat(np.arange(NUM_INPUT), num_spikes_per_neuron)
+
+# Pre-process spikes
+in_spikes = preprocess_spikes(in_spike_times.flatten(), in_spike_ids, NUM_INPUT)
+
+network = Network()
+with network:
+    # Populations
+    input = Population(SpikeInput(max_spikes=len(in_spike_ids)),
+                                  NUM_INPUT)
+    hidden = Population(LeakyIntegrateFire(v_thresh=0.61, tau_mem=20.0,
+                                           tau_refrac=5.0, 
+                                           relative_reset=True,
+                                           integrate_during_refrac=True),
+                        NUM_HIDDEN)
+    output = Population(LeakyIntegrate(tau_mem=20.0, output="var"),
+                        NUM_OUTPUT)
+    
+    # Connections
+    Connection(input, hidden, Dense(Normal(sd=1.0 / np.sqrt(NUM_INPUT))))
+    Connection(hidden, hidden, Dense(Normal(sd=1.0 / np.sqrt(NUM_HIDDEN))))
+    Connection(hidden, output, Dense(Normal(sd=1.0 / np.sqrt(NUM_INPUT))))
+
+compiler = EPropCompiler(example_timesteps=1000, losses="mean_square_error",
+                         optimiser=Adam(), c_reg=3.0)
+compiled_net = compiler.compile(network)
+
+with compiled_net:
+    # Evaluate model on numpy dataset
+    start_time = perf_counter()
+    metrics, _ = compiled_net.train({input: in_spikes},
+                                    {output: y_star},
+                                    num_epochs=1000,
+                                    metrics="mean_square_error")
+    end_time = perf_counter()
+    print(f"Accuracy = {metrics[output].result}")
+    print(f"Time = {end_time - start_time}s")
