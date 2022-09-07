3c4351fd40465843a512954dcaedbf455ef867b6
neworderofjamie <neworderofjamie@gmail.com>
2022-09-07 11:29:00 +0100

    functionality in Model to handle and replace variable/variable reference access mode

--- ml_genn/ml_genn/utils/model.py
+++ ml_genn/ml_genn/utils/model.py
@@ -1,4 +1,5 @@
 from textwrap import dedent
+from pygenn.genn_wrapper.Models import VarAccessMode_READ_WRITE
 
 class Model:
     def __init__(self, model, param_vals={}, var_vals={}, egp_vals={}):
@@ -12,13 +13,17 @@ class Model:
         self._add_to_list("param_name_types", (name, type))
         self.param_vals[name] = value
 
-    def add_var(self, name, type, value):
-        self._add_to_list("var_name_types", (name, type))
+    def add_var(self, name, type, value,
+                access_mode=VarAccessMode_READ_WRITE):
+        self._add_to_list("var_name_types", (name, type, access_mode))
         self.var_vals[name] = value
 
     def add_egp(self, name, type, value):
         self._add_to_list("extra_global_params", (name, type))
         self.egp_vals[name] = value
+    
+    def set_var_access_mode(self, name, access_mode):
+        self._set_access_model("var_name_types", name, access_mode)
 
     def _add_to_list(self, name, value):
         if name not in self.model:
@@ -31,6 +36,17 @@ class Model:
             self.model[name] = f"{code}\n"
         else:
             self.model[name] += f"\n{code}\n"
+    
+    def _set_access_model(self, name, var, access_mode):
+        # Find var
+        var_array = self.model[name]
+        var_index = [i for i, v in enumerate(var_array)
+                     if v[0] == var]
+        assert len(var_index) == 1
+        var_index = var_index[0]
+        
+        # Take first two elements of existing var and add access mode
+        var_array[var_index] = var_array[var_index][:2] + (access_mode,)
 
 class CustomUpdateModel(Model):
     def __init__(self, model, param_vals={}, var_vals={}, 
@@ -43,6 +59,9 @@ class CustomUpdateModel(Model):
     def add_var_ref(self, name, type, value):
         self._add_to_list("var_refs", (name, type))
         self.var_refs[name] = value
+    
+    def set_var_ref_access_mode(self, name, access_mode):
+        self._set_access_model("var_refs", name, access_mode)
 
     def append_update_code(self, code):
         self._append_code("update_code", code)
