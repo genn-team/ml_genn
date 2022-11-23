278d39c9f0d2e5b4de41e44db2223133ce2f065e
neworderofjamie <neworderofjamie@gmail.com>
2022-11-23 11:44:02 +0000

    tidied SHD model a little

--- ml_genn_eprop/examples/shd_classifier.py
+++ ml_genn_eprop/examples/shd_classifier.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 from ml_genn import Connection, Population, Network
+from ml_genn.callbacks import Checkpoint
 from ml_genn.connectivity import Dense
 from ml_genn.initializers import Normal
 from ml_genn.neurons import (LeakyIntegrate, AdaptiveLeakyIntegrateFire,
                              SpikeInput)
+from ml_genn.serialisers import Numpy
 from ml_genn_eprop import EPropCompiler
 from tonic.datasets import SHD
 
@@ -14,9 +16,13 @@ from ml_genn.utils.data import (calc_latest_spike_time, calc_max_spikes,
 
 NUM_HIDDEN = 256
 BATCH_SIZE = 512
+NUM_EPOCHS = 50
+
+TRAIN = True
+KERNEL_PROFILING = False
 
 # Get SHD dataset
-dataset = SHD(save_to='./data', train=True)
+dataset = SHD(save_to='./data', train=TRAIN)
 
 # Preprocess
 spikes = []
@@ -36,6 +42,7 @@ print(f"Max spikes {max_spikes}, latest spike time {latest_spike_time}")
 num_input = int(np.prod(dataset.sensor_size))
 num_output = len(dataset.classes)
 
+serialiser = Numpy("shd_checkpoints")
 network = Network()
 with network:
     # Populations
@@ -46,28 +53,52 @@ with network:
                                                    relative_reset=True,
                                                    integrate_during_refrac=True),
                         NUM_HIDDEN)
+    hidden2 = Population(AdaptiveLeakyIntegrateFire(v_thresh=0.6,
+                                                    tau_refrac=5.0,
+                                                    relative_reset=True,
+                                                    integrate_during_refrac=True),
+                         NUM_HIDDEN)
     output = Population(LeakyIntegrate(tau_mem=20.0, readout="sum_var", softmax=True),
                         num_output)
 
     # Connections
     Connection(input, hidden, Dense(Normal(sd=1.0 / np.sqrt(num_input))))
     Connection(hidden, hidden, Dense(Normal(sd=1.0 / np.sqrt(NUM_HIDDEN))))
-    Connection(hidden, output, Dense(Normal(sd=1.0 / np.sqrt(NUM_HIDDEN))))
+    Connection(hidden, hidden2, Dense(Normal(sd=1.0 / np.sqrt(NUM_HIDDEN))))
+    Connection(hidden2, hidden2, Dense(Normal(sd=1.0 / np.sqrt(NUM_HIDDEN))))
+    Connection(hidden2, output, Dense(Normal(sd=1.0 / np.sqrt(NUM_HIDDEN))))
 
-compiler = EPropCompiler(example_timesteps=int(np.ceil(latest_spike_time)),
-                         losses="sparse_categorical_crossentropy", 
-                         optimiser="adam", batch_size=BATCH_SIZE)
-compiled_net = compiler.compile(network)
+if TRAIN:
+    compiler = EPropCompiler(example_timesteps=int(np.ceil(latest_spike_time)),
+                             losses="sparse_categorical_crossentropy", 
+                             optimiser="adam", batch_size=BATCH_SIZE)
+    compiled_net = compiler.compile(network)
 
-with compiled_net:
-    # Evaluate model on SHD
-    start_time = perf_counter()
-    callbacks = ["batch_progress_bar", "checkpoint"]
-    metrics, cb_data  = compiled_net.train({input: spikes},
-                                           {output: labels},
-                                           num_epochs=50,
-                                           callbacks=callbacks,
-                                           shuffle=True)
-    end_time = perf_counter()
-    print(f"Accuracy = {100 * metrics[output].result}%")
-    print(f"Time = {end_time - start_time}s")
+    with compiled_net:
+        # Evaluate model on SHD
+        start_time = perf_counter()
+        callbacks = ["batch_progress_bar", "checkpoint"]
+        metrics, _  = compiled_net.train({input: spikes},
+                                         {output: labels},
+                                         num_epochs=50,
+                                         callbacks=callbacks,
+                                         shuffle=True)
+        end_time = perf_counter()
+        print(f"Accuracy = {100 * metrics[output].result}%")
+        print(f"Time = {end_time - start_time}s")
+else:
+    # Load network state from final checkpoint
+    network.load((NUM_EPOCHS - 1,), Checkpoint(serialiser))
+
+    compiler = InferenceCompiler(evaluate_timesteps=max_example_timesteps,
+                                 batch_size=BATCH_SIZE)
+    compiled_net = compiler.compile(network)
+
+    with compiled_net:
+        # Evaluate model on numpy dataset
+        start_time = perf_counter()
+        metrics, _  = compiled_net.evaluate({input: spikes},
+                                            {output: labels})
+        end_time = perf_counter()
+        print(f"Accuracy = {100 * metrics[output].result}%")
+        print(f"Time = {end_time - start_time}s")
