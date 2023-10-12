25f754f0d667d6ddc5c78b1f0a43657b8653bd85
neworderofjamie <neworderofjamie@gmail.com>
2023-10-12 14:09:01 +0100

    MPI communicator fixes

--- ml_genn/ml_genn/communicators/mpi.py
+++ ml_genn/ml_genn/communicators/mpi.py
@@ -1,27 +1,28 @@
+import logging
+
 from .communicator import Communicator
 
-try:
-    from mpi4py import MPI
-except ImportError:
-    _has_mpi4py = False
-else:
-    _has_mpi4py = True
-    
+logger = logging.getLogger(__name__)
+
 class MPI(Communicator):
     """Implementation of Communicator which uses mpi4py 
     for parallel communications between ranks
     """
     def __init__(self):
-        if not _has_mpi4py:
+        try:
+            from mpi4py import MPI
+        except ImportError:
             raise ImportError("mpi4py is required to use MPI communicator")
-        
+
         # Get communicator
         self.comm = MPI.COMM_WORLD
 
         # Get our rank and number of ranks
         self._rank = self.comm.Get_rank()
         self._num_ranks = self.comm.Get_size()
-        
+
+        logger.info(f"MPI communicator {self._rank}/{self._num_ranks}")
+
     def barrier(self):
         """Wait for all ranks to reach this point in execution before continuing
         """
@@ -42,8 +43,9 @@ class MPI(Communicator):
         Returns:
             Sum of value across all ranks
         """
-        return self.comm.allreduce(sendobj=value, op=MPI.SUM)
+        return self.comm.allreduce(sendobj=value)
 
+    @property
     def rank(self):
         """Gets index of this rank
 
@@ -52,10 +54,11 @@ class MPI(Communicator):
         """
         return self._rank
 
+    @property
     def num_ranks(self):
         """Gets total number of ranks
 
         Returns:
             int: num_ranks
         """
-        return self._rank
+        return self._num_ranks
