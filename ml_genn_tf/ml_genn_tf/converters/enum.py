70a777271aa03a4494d4586b5907298a53253937
neworderofjamie <neworderofjamie@gmail.com>
2024-04-10 13:51:14 +0100

    tweaked mlGeNN TF module structure

--- ml_genn_tf/ml_genn_tf/converters/enum.py
+++ ml_genn_tf/ml_genn_tf/converters/enum.py
@@ -4,9 +4,3 @@ class InputType(Enum):
     SPIKE = 'spike'
     POISSON = 'poisson'
     IF = 'if'
-
-class ConverterType(Enum):
-    SIMPLE = 'simple'
-    DATA_NORM = 'data-norm'
-    SPIKE_NORM = 'spike-norm'
-    FEW_SPIKE = 'few-spike'
