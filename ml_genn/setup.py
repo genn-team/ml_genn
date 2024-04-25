d51d9761718095d38464aca98ad6380730b51c96
neworderofjamie <neworderofjamie@gmail.com>
2024-04-25 15:39:45 +0100

    updated version

--- ml_genn/setup.py
+++ ml_genn/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="ml_genn",
-      version="2.1.0",
+      version="2.2.0",
       packages=find_packages(),
 
       python_requires=">=3.7.0",
