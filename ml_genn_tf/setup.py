1a27df5c561f40a70783a4b43ff76f0b54e22dbd
neworderofjamie <neworderofjamie@gmail.com>
2024-06-25 10:38:28 +0100

    updated version

--- ml_genn_tf/setup.py
+++ ml_genn_tf/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="ml_genn_tf",
-    version="2.2.0",
+    version="2.2.1",
     packages=find_packages(),
 
     python_requires=">=3.7.0",
     install_requires = [
         "tensorflow<2.15.0",
-        "ml_genn>=2.2.0,<3.0.0"])
+        "ml_genn>=2.2.1,<3.0.0"])
