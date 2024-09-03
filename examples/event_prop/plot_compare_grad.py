a63ff1f7398c9a6f0b2219e29ec1a1786ea01e3d
neworderofjamie <neworderofjamie@gmail.com>
2024-09-03 09:46:21 +0100

    modified example and plotting scripts

---
+++ examples/event_prop/plot_compare_grad.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_grads(ml_genn_filename, thomas_filename):
+    grad = np.load(ml_genn_filename)
+    grad = np.reshape(grad, grad.shape[:2] + (-1,))
+
+    thomas_grad = np.load(thomas_filename)
+    thomas_grad = np.reshape(thomas_grad, grad.shape)
+    
+    num_viz = 20
+    synapse_inds = np.random.choice(grad.shape[-1], num_viz)
+    fig, axes = plt.subplots(num_viz, 6, sharex="col", sharey="row")
+    for i in range(6):
+        axes[0,i].set_title(f"Trial {i}")
+        
+        for j in range(num_viz):
+            ml_genn_actor = axes[j,i].plot(grad[i,:,synapse_inds[j]], alpha=0.5)[0]
+            thomas_actor = axes[j,i].plot(grad[i,:,synapse_inds[j]], alpha=0.5)[0]
+
+    fig.legend([ml_genn_actor, thomas_actor], ["mlGeNN", "Thomas"],
+               ncol=2, loc="lower center")
+    fig.tight_layout(pad=0)
+   
+    return fig, axes
+
+in_hid_fig, in_hid_axes = plot_grads("in_hid_grad_hack.npy", "COMPARE_HACK_FABS_dwin_to_hid.npy")
+in_hid_fig.suptitle("Input to hidden gradients")
+
+hid_out_fig, hid_out_axes = plot_grads("hid_out_grad_hack.npy", "COMPARE_HACK_FABS_dwhid_to_out.npy")
+hid_out_fig.suptitle("Hidden to output gradients")
+plt.show()
---
+++ examples/event_prop/plot_compare_weight.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_weights(ml_genn_filename, thomas_filename):
+    in_hid_weight = np.load(ml_genn_filename)
+    
+
+    thomas_in_hid_weight = np.load(thomas_filename)
+    thomas_in_hid_weight = np.reshape(thomas_in_hid_weight, in_hid_weight.shape)
+    
+    in_hid_weight = in_hid_weight[:,0,:,:]
+    thomas_in_hid_weight = thomas_in_hid_weight[:,0,:,:]
+    weight_diff = np.abs(in_hid_weight - thomas_in_hid_weight)
+    min_weight = min(np.amin(in_hid_weight), np.amin(thomas_in_hid_weight))
+    max_weight = max(np.amax(in_hid_weight), np.amax(thomas_in_hid_weight))
+
+    fig, axes = plt.subplots(3, 6, sharex="col", sharey="row")
+    for i in range(6):
+        axes[0,i].set_title(f"Trial {i}")
+        axes[0,i].imshow(in_hid_weight[i], aspect="auto", vmin=min_weight, vmax=max_weight)
+        axes[1,i].imshow(thomas_in_hid_weight[i], aspect="auto", vmin=min_weight, vmax=max_weight)
+        axes[2,i].imshow(weight_diff[i], aspect="auto")
+
+    axes[0,0].set_ylabel("mlGeNN")
+    axes[1,0].set_ylabel("Thomas")
+    axes[2,0].set_ylabel("Difference")
+    return fig, axes
+
+in_hid_fig, in_hid_axes = plot_weights("in_hid_g_hack.npy", "COMPARE_HACK_FABS_win_to_hid.npy")
+in_hid_fig.suptitle("Input to hidden weights")
+
+hid_out_fig, hid_out_axes = plot_weights("hid_out_g_hack.npy", "COMPARE_HACK_FABS_whid_to_out.npy")
+hid_out_fig.suptitle("Hidden to output weights")
+plt.show()
