341649857721ef9d771b93786c9de1bf42f30cfc
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2024-11-11 10:56:33 +0000

    Some more realistic example values for regularisation and clamping.

--- examples/event_prop/shd_augment.py
+++ examples/event_prop/shd_augment.py
@@ -100,22 +100,23 @@ with network:
                         num_output)
 
     # Connections
-    Connection(input, hidden, Dense(Normal(mean=0.03, sd=0.01)),
+    in_to_hid = Connection(input, hidden, Dense(Normal(mean=0.03, sd=0.01)),
                Exponential(5.0))
     hid_to_hid = Connection(hidden, hidden, Dense(Normal(mean=0.0, sd=0.02)),
                Exponential(5.0))
-    Connection(hidden, output, Dense(Normal(mean=0.0, sd=0.03)),
+    hid_to_out = Connection(hidden, output, Dense(Normal(mean=0.0, sd=0.03)),
                Exponential(5.0))
 
 max_example_timesteps = int(np.ceil(latest_spike_time / DT))
 if TRAIN:
     compiler = EventPropCompiler(example_timesteps=max_example_timesteps,
                                  losses="sparse_categorical_crossentropy",
-                                 reg_lambda_upper=4e-09, reg_lambda_lower=4e-09, 
+                                 reg_lambda_upper=2.5e-10, reg_lambda_lower=2.5e-10, 
                                  reg_nu_upper=14, max_spikes=1500, 
                                  optimiser=Adam(0.001), batch_size=BATCH_SIZE, 
                                  kernel_profiling=KERNEL_PROFILING,
-                                 clamp_weight_conns= {hid_to_hid: (-0.2,0.2)},
+                                 clamp_weight_conns= {in_to_hid: (-1.,1.),
+                                 hid_to_hid: (-1.,1.),hid_to_out: (-1.,1.)},
     )
     compiled_net = compiler.compile(network)
 
