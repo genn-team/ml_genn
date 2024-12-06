6f6b2f85b4bf8a58e87302f32cc8a9ee378f8148
Thomas Nowotny <tn41@atlas.inf.susx.ac.uk>
2024-12-06 16:19:38 +0000

    Simplified test1.py to have a more clean proof of concept to work from.

--- ml_genn/ml_genn/test1.py
+++ ml_genn/ml_genn/test1.py
@@ -1,12 +1,7 @@
 import sympy
 from sympy.parsing.sympy_parser import parse_expr
+from auto_tools import *
 
-def add(o, expr):
-    if o is None:
-        return expr
-    else:
-        return o+expr
-    
 DEBUG = False
 
 varname= [ "V", "I" ]
@@ -26,331 +21,181 @@ jumps = {
     "I": "I+w"           # I value after synaptic jump
 }
 
-all_sym = {}
-x = []
-for vname in varname:
-    all_sym[vname] = sympy.Symbol(vname)
-    x.append(all_sym[vname])
+sym = {}
+for var in varname:
+    sym[var] = sympy.Symbol(var)
 
-p = []
 for pname in parname:
-    all_sym[pname] = sympy.Symbol(pname)
-    p.append(all_sym[pname])
+    sym[pname] = sympy.Symbol(pname)
 
-all_sym[w_name] = sympy.Symbol(w_name)
-w = all_sym[w_name]
+sym[w_name] = sympy.Symbol(w_name)
 
 dt= sympy.Symbol("dt")
 
 # forward dynamics
-dx_dt = []
+dx_dt = {}
 for var in varname:
     if var in eqns:
-        dx_dt.append(parse_expr(eqns[var],local_dict= all_sym))
-    else:
-        dx_dt.append(None)
+        dx_dt[var] = parse_expr(eqns[var],local_dict= sym)
         
 # threshold condition
 g = parse_expr(threshold)
 
 # reset function
-f = []
+f = {}
 for var in varname:
     if var in reset:
-        f.append(parse_expr(reset[var],local_dict= all_sym))
-    else:
-        f.append(None)
+        f[var] = parse_expr(reset[var],local_dict= sym)
         
 # synaptic jumps
-h = []
-for i,var in enumerate(varname):
+h = {}
+for var in varname:
     if var in jumps:
-        tmp = parse_expr(jumps[var],local_dict= all_sym)-x[i]
-        if sympy.diff(tmp, x[i]) == 0:
-            h.append(tmp)
+        tmp = parse_expr(jumps[var],local_dict= sym)-sym[var]
+        if sympy.diff(tmp, sym[var]) == 0:
+            h[var] = tmp
         else:
             raise NotImplementedError(
                 "EventProp compiler only supports "
                 "synapses which (only) add input to a target variable.")
-
-    else:
-        h.append(None)
-        
-
 # adjoint variables
-adj_name = [ f"lambda_{var}" for var in varname ]
-
-adj = []
-for a in adj_name:
-    all_sym[a] = sympy.symbols(a)
-    adj.append(all_sym[a])
+adj_name = {}
+for var in varname:
+    adj_name[var] = f"lambda_{var}"
+    sym[adj_name[var]] = sympy.Symbol(adj_name[var])
     
 # after jump dynamics equation "\dot{x}^+"
-dx_dtplusn = []
-for j in range(len(dx_dt)):
-    plus = dx_dt[j]
-    for i in range(len(x)):
-        plus = plus.subs(x[i],f[i])
-    dx_dtplusn.append(plus)
+dx_dtplusn = {}
+for var, expr in dx_dt.items():
+    plus = expr
+    for v2, f2 in f.items():
+        plus = plus.subs(sym[v2],f2)
+        dx_dtplusn[var] = plus
 
-dx_dtplusm = []
-for j in range(len(dx_dt)):
-    plus = dx_dt[j]
-    for i in range(len(x)):
-        plus = plus.subs(x[i],x[i]+h[i])
-    dx_dtplusm.append(plus)
-    
+dx_dtplusm = {}
+for var, expr in dx_dt.items():
+    plus = expr
+    for v2, h2 in h.items():
+        plus = plus.subs(sym[v2],sym[v2]+h2)
+    dx_dtplusm[var] = plus
+
 # generate the adjoint dynamics
-ode = []
-for r in range(len(x)):
-    o = None
-    for k in range(len(adj)):
-        #print(f"dx_dt[k]: {dx_dt[k]} x[r]: {x[r]} diff: {sympy.diff(dx_dt[k], x[r])}")
-        o = add(o, sympy.diff(dx_dt[k], x[r])*adj[k])
+ode = {}
 
-    ode.append(o)
+for var in varname:
+    o = None
+    for v2, expr in dx_dt.items():
+        o = add(o, sympy.diff(expr, sym[var])*sym[adj_name[v2]])
+
+    ode[var] = o
 
 # generate the jumps in lambda^n adjoint variables, n == spiking neuron
 
 # first, calculate all terms involving lambda^n+
-expr = [[] for i in range(5)]
-for r in range(len(adj)): 
+expr = [{} for i in range(5)]
+for var in varname:
     ex= None
-    for s in range(len(f)):
-        ex = add(ex, sympy.diff(f[s],x[r])*adj[s])
-    expr[0].append(ex)
+    for v2,f2 in f.items():
+        ex = add(ex, sympy.diff(f2,sym[var])*sym[adj_name[v2]])
+    expr[0][var] = ex
 
     ex= None
-    for s in range(len(x)):
-        ex = add(ex, sympy.diff(g,x[s])*dx_dt[s])
+    for v2 in varname:
+        ex = add(ex, sympy.diff(g,sym[v2])*dx_dt[v2])
     ex = sympy.simplify(ex)
     
-    if ex == 0:
-        expr[1].append[None]
-    else:
-        ex = sympy.diff(g,x[r])/ex
-        if ex == 0:
-            expr[1].append(None)
-        else:
-            expr[1].append(ex)
+    if ex != 0:
+        ex = sympy.diff(g,sym[var])/ex
+        if ex != 0:
+            expr[1][var] = ex
         
     # TODO: dlp/dtk + lk- + lk+ here
 
-    if (expr[1][r] != None):
+    if var in expr[1]:
         ex= None
-        for s in range(len(x)):
+        for v2,f2 in f.items():
             ex2= None
-            for q in range(len(x)):
-                ex2 = add(ex2, sympy.diff(f[s],x[q])*dx_dt[q])
-            ex2 = add(ex2,-dx_dtplusn[s])
-            ex = add(ex,-adj[s]*ex2)
-        expr[2].append(ex)
-    else:
-        expr[2].append(None)
+            for v3 in varname:
+                ex2 = add(ex2, sympy.diff(f2,sym[v3])*dx_dt[v3])
+            ex2 = add(ex2,-dx_dtplusn[v2])
+            ex = add(ex,-sym[adj_name[v2]]*ex2)
+        expr[2][var] = ex
 
 # assemble the lambda^n+ parts
-jumps = []
-for r in range(len(adj)):
+jumps = {}
+for var in varname:
     ex = None
-    ex = add(ex, expr[0][r]) 
-    if expr[1][r] is not None and expr[2][r] is not None:
-        ex = add(ex, expr[1][r] * expr[2][r])
-    jumps.append(ex)
+    ex = add(ex, expr[0][var])
+    if var in expr[1] and var in expr[2]:
+        ex = add(ex, expr[1][var] * expr[2][var])
+    jumps[var] = ex
 
 # Second, calculate all the terms related to other lambda^m+
 # These are "addToPre" situations
 
 # the term due to the potential x-dependence of the synaptic jumps h
-for r in range(len(adj)):
+
+
+for var in varname:
     ex = None
-    for s in range(len(x)):
-        ex = add(ex, sympy.diff(h[s],x[s])*adj[r])
-    expr[3].append(ex)
+    for v2,h2 in h.items():
+        ex = add(ex, sympy.diff(h2,sym[v2])*sym[adj_name[var]])
+    if ex is not None:
+        expr[3][var] = ex
 
 # the term due to the w dependence of the synaptic jumps h
-for r in range(len(adj)):
+for var in varname:
     ex = None
-    for s in range(len(adj)):
+    for v2, expr2 in dx_dt.items():
         ex2 = None
-        ex2 = add(ex2, dx_dtplusm[s] - dx_dt[s])
-        for q in range(len(x)):
-            ex2 = add(ex2, sympy.diff(h[s],x[q])*dx_dt[q])
-        ex = add(ex, adj[s]*ex2)
-    expr[4].append(sympy.simplify(ex))
+        ex2 = add(ex2, dx_dtplusm[v2] - expr2)
+        if v2 in h:
+            for v3, expr3 in dx_dt.items():
+                ex2 = add(ex2, sympy.diff(h[v2],sym[v3])*expr3)
+        ex = add(ex, sym[adj_name[v2]]*ex2)
+    expr[4][var] = sympy.simplify(ex)
 
 # assemble add_to_pre
-add_to_pre = []
-for r in range(len(adj)):
+add_to_pre = {}
+for var in varname:
     ex = None
-    ex = add(ex, expr[3][r])
-    if expr[1][r] is not None and expr[4][r] is not None:
-        ex = add(ex, expr[1][r] * expr[4][r])
-    add_to_pre.append(sympy.simplify(ex))
-    
+    if var in expr[3]:
+        ex = add(ex, expr[3][var])
+    if var in expr[1] and var in expr[4]:
+        ex = add(ex, expr[1][var] * expr[4][var])
+    if ex is not None:
+        add_to_pre[var] = sympy.simplify(ex)
+
 if DEBUG:
     for i in range(len(expr)):
         print(f"expression {i}:")
         for ex in expr[i]:
             print(ex)
-    
-# the values that need to be saved in the forward pass
-# TODO
-
-
-"""
-THis function turns ODEs expressed as two lists for sympy variables and matching rhs expressions 
-into C code to update the variables with timestep "dt"
-"""
-
-def linear_euler(vars,exprs):
-    code = []
-    for var,expr in zip(vars,exprs):
-        code.append(sympy.ccode(var+expr*dt, assign_to= f"const scalar {str(var)}_tmp"))
-    for var in vars:
-        name = str(var)
-        code.append(f"{name} = {name}_tmp")
-    return code
-
-    
-""" 
-This modified from Brian 2's exponential_euler stateupdater
-"""
-
-def get_conditionally_linear_system(vars, exprs):
-    """
-    Convert equations into a linear system using sympy.
-
-    Parameters
-    ----------
-    eqs : `Equations`
-        The model equations.
-
-    Returns
-    -------
-    coefficients : dict of (sympy expression, sympy expression) tuples
-        For every variable x, a tuple (M, B) containing the coefficients M and
-        B (as sympy expressions) for M * x + B
-
-    Raises
-    ------
-    ValueError
-        If one of the equations cannot be converted into a M * x + B form.
-    """
-
-    coefficients = {}
-    for var, expr in zip(vars,exprs):
-        name = str(var)
-        if expr.has(var):
-            # Factor out the variable
-            expr = expr.expand()
-            expr = sympy.collect(expr, var, evaluate=False)
-            if len(expr) > 2 or var not in expr:
-                raise ValueError(
-                    f"The expression '{expr}', defining the variable "
-                    f"'{name}', could not be separated into linear "
-                    "components."
-                )
-            coefficients[name] = (expr[var], expr.get(1, 0))
-        else:
-            coefficients[name] = (0, expr)
-
-    return coefficients
-
-
-def exponential_euler(vars,exprs):
-    # Try whether the equations are conditionally linear
-    try:
-        system = get_conditionally_linear_system(vars,exprs)
-    except ValueError:
-        raise NotImplementedError(
-            "Can only solve conditionally linear systems with this state updater."
-        )
-
-    code = []
-    for var, (A, B) in system.items():
-        s_var = sympy.Symbol(var)
-        s_dt = sympy.Symbol("dt")
-        if A == 0:
-            update_expression = s_var + s_dt * B
-        elif B != 0:
-            BA = B / A
-            # Avoid calculating B/A twice
-            BA_name = f"BA_{var}_tmp"
-            s_BA = sympy.Symbol(BA_name)
-            code += [f"const scalar {BA_name} = {sympy.ccode(BA)};"]
-            update_expression = (s_var + s_BA) * sympy.exp(A * s_dt) - s_BA
-        else:
-            update_expression = s_var * sympy.exp(A * s_dt)
-            
-        # The actual update step
-        code += [f"const scalar {var}_tmp = {sympy.ccode(update_expression)};"]
-
-    # Replace all the variables with their updated value
-    for var in system:
-        code += [f"{var} = {var}_tmp;"]
-
-    return code
-
-"""
-End of Brian 2 modified code
-"""
-
-def saved_vars(x, adj_ode, adj_jump, add_to_pre):
-    saved = set()
-    all = adj_ode + adj_jump + add_to_pre
-    for expr in all:
-        for var in x:
-            if expr.has(var):
-                saved.add(var)
-
-    return saved
-
-# one could reduce saved vars by solving the threshold equation for one of the vars and substituting teh equation
-def simplify_using_threshold(x, g, adj_jump, add_to_pre):
-    the_var = None
-    for var in x:
-        if g.has(var):
-            the_var = var
-            break
-
-    if the_var is None:
-        return adj_jump, add_to_pre
-
-    sln = sympy.solve(g, the_var)
-
-    if len(sln) != 1:
-        return adj_jump, add_to_pre
-    
-    new_adj_jump = []
-    for expr in adj_jump:
-        new_adj_jump.append(expr.subs(the_var,sln[0]))
-    new_add_to_pre = []
-    for expr in add_to_pre:
-        new_add_to_pre.append(expr.subs(the_var,sln[0]))
-    return  new_adj_jump, new_add_to_pre
         
-eeuler = exponential_euler(x,dx_dt)
-eeuler_adj = exponential_euler(adj,ode)
+eeuler = exponential_euler(varname, sym, dx_dt, dt)
+adj = [ adj_name[var] for var in varname ]
+eeuler_adj = exponential_euler(adj, sym, ode, dt)
 
-lineuler = linear_euler(x,dx_dt)
-lineuler_adj = linear_euler(adj,ode)
+lineuler = linear_euler(varname, sym, dx_dt, dt)
+lineuler_adj = linear_euler(adj, sym, ode, dt)
 
-jumps, add_to_pre= simplify_using_threshold(x, g, jumps, add_to_pre)
+jumps, add_to_pre= simplify_using_threshold(varname, sym, g, jumps, add_to_pre)
 
 jump_c = []
 updated = []
-for i,j in enumerate(jumps):
-    if sympy.simplify(j - adj[i]) != 0:
-        jump_c.append(sympy.ccode(j, assign_to= f"const scalar {adj_name[i]}_tmp"))
-        updated.append(adj[i])
-for var in updated:
-    jump_c += [f"{var} = {var}_tmp;"]
+for var in varname:
+    if var in jumps:
+        if sympy.simplify(jumps[var] - sym[adj_name[var]]) != 0:
+            jump_c.append(sympy.ccode(jumps[var], assign_to= f"const scalar {adj_name[var]}_tmp"))
+            updated.append(adj_name[var])
+for adj in updated:
+    jump_c += [f"{adj} = {adj}_tmp;"]
 
 add_to_pre_c = []
-for i,a in enumerate(add_to_pre):
-    if a != 0:
-        code = sympy.ccode(a)
-        add_to_pre_c.append(f"addToPre({adj_name[i]},{code});")
+for var in varname:
+    if var in add_to_pre and add_to_pre[var] != 0:
+        code = sympy.ccode(add_to_pre[var])
+        add_to_pre_c.append(f"addToPre({adj_name[var]},{code});")
     
 print("Exponential Euler:")
 print("\n".join(eeuler))
@@ -362,7 +207,7 @@ print("\n".join(lineuler_adj))
 
 
 print("Saved vars:")
-print(saved_vars(x, ode, jumps, add_to_pre))
+print(saved_vars(varname, sym, ode, jumps, add_to_pre))
 
 print("Adjoint jumps:")
 for code in jump_c:
