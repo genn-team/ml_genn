2a5547d918051107950bfbea05412fb5b18d0a9f
neworderofjamie <neworderofjamie@gmail.com>
2025-02-11 09:50:18 +0000

    remove e-prop compiler and add auto model

--- ml_genn/ml_genn/compilers/__init__.py
+++ ml_genn/ml_genn/compilers/__init__.py
@@ -8,11 +8,10 @@ used to interact with the GeNN models.
 from .compiler import Compiler
 from .compiled_network import CompiledNetwork
 from .compiled_training_network import CompiledTrainingNetwork
-from .eprop_compiler import EPropCompiler
 from .event_prop_compiler import EventPropCompiler
 from .few_spike_compiler import CompiledFewSpikeNetwork, FewSpikeCompiler
 from .inference_compiler import CompiledInferenceNetwork, InferenceCompiler
 
 __all__ = ["Compiler", "CompiledFewSpikeNetwork", "CompiledInferenceNetwork",
-           "CompiledNetwork", "CompiledTrainingNetwork", "EPropCompiler", 
-           "EventPropCompiler", "FewSpikeCompiler", "InferenceCompiler"]
+           "CompiledNetwork", "CompiledTrainingNetwork", "EventPropCompiler",
+           "FewSpikeCompiler", "InferenceCompiler"]
