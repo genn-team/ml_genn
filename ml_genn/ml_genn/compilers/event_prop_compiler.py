95942112b78cd7aa056af234e91e2c0c80c19a2b
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2026-05-28 18:18:44 +0100

    Added a signed latency convolutional example.

--- ml_genn/ml_genn/compilers/event_prop_compiler.py
+++ ml_genn/ml_genn/compilers/event_prop_compiler.py
@@ -811,6 +811,9 @@ class EventPropCompiler(Compiler):
         compile_state.add_synapse_reset_vars(conn, genn_model.reset_vars)
         
         # Return model
+        for x,y in genn_model.model.items():
+            print(f"{x}; {y}")
+
         return genn_model
 
     def build_weight_update_model(self, conn: Connection,
@@ -1137,6 +1140,8 @@ class EventPropCompiler(Compiler):
             # Convert expression to c-code and insert call to addToPre
             genn_model.append_pre_event_syn_code(f"addToPre({dx_dt_diff_sum_code});")
 
+        for x,y in genn_model.model.items():
+            print(f"{x}; {y}")
         return genn_model
 
     def create_compiled_network(self, genn_model, neuron_populations: dict,
@@ -1894,6 +1899,8 @@ class EventPropCompiler(Compiler):
                 strict_check=(neuron_reset_strict_check
                                 if self.strict_buffer_checking
                                 else "")))
+        for x,y in genn_model.model.items():
+            print(f"{x}; {y}")
         return genn_model
         
     def _build_out_neuron_model(self, pop: Population, 
