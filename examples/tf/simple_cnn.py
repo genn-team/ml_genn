e5d351ff484a579146d9af3651918acef191e92e
Danilo Cavalcanti <danilo-bc@users.noreply.github.com>
2026-06-15 18:00:01 -0300

    Add snippet as a confirmation of 'predict' from FewSpikeCompiler

--- examples/tf/simple_cnn.py
+++ examples/tf/simple_cnn.py
@@ -103,8 +103,17 @@ with compiled_net:
         {net_inputs[0]: validate_x}, {net_outputs[0]: validate_y},
         callbacks=callbacks)
     end_time = perf_counter()
-    print(f"Accuracy = {100.0 * metrics[net_outputs[0]].result}%")
-    print(f"Time = {end_time - start_time} s")
+    print(f"Accuracy from metrics = {100.0 * metrics[net_outputs[0]].result}%")
+    print(f"FewSpikeCompiler 'evaluate' time = {end_time - start_time} s")
+
+    start_time = perf_counter()
+    all_y_pred, cb_data = compiled_net.predict(
+        {net_inputs[0]: validate_x}, {net_outputs[0]: validate_y},
+        callbacks=callbacks)
+    end_time = perf_counter()
+    y_pred_accuracy = np.count_nonzero(np.argmax(all_y_pred, axis=1) == validate_y) / len(validate_y)
+    print(f"Accuracy from manual calc. from predict = {100.0 * y_pred_accuracy}%")
+    print(f"FewSpikeCompiler 'predict' time = {end_time - start_time} s")
     
     if args.kernel_profiling:
         reset_time = compiled_net.genn_model.get_custom_update_time("Reset")
